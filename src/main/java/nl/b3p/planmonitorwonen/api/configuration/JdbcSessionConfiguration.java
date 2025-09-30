e3e34e2b4508169b2c39197e3e0a6098d9356937
Matthijs Laan <matthijsln@gmail.com>
2025-10-01 00:16:37 +0200

    Ignore deserialization exceptions for JDBC sessions

--- src/main/java/nl/b3p/planmonitorwonen/api/configuration/JdbcSessionConfiguration.java
+++ src/main/java/nl/b3p/planmonitorwonen/api/configuration/JdbcSessionConfiguration.java
@@ -6,6 +6,8 @@
 
 package nl.b3p.planmonitorwonen.api.configuration;
 
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,11 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.core.serializer.Deserializer;
+import org.springframework.core.serializer.support.DeserializingConverter;
+import org.springframework.core.serializer.support.SerializingConverter;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.jdbc.config.annotation.SpringSessionDataSource;
@@ -74,5 +81,24 @@ public class JdbcSessionConfiguration {
   public PlatformTransactionManager springSessionTransactionOperations(
       @Qualifier("springSessionDataSource") DataSource springSessionDatasource) {
     return new DataSourceTransactionManager(springSessionDatasource);
+  }
+
+  @Bean("springSessionConversionService")
+  public ConversionService springSessionConversionService() {
+    GenericConversionService converter = new GenericConversionService();
+    converter.addConverter(Object.class, byte[].class, new SerializingConverter());
+    converter.addConverter(byte[].class, Object.class, new DeserializingConverter(new CustomDeserializer()));
+    return converter;
+  }
+
+  static class CustomDeserializer implements Deserializer<Object> {
+    @Override
+    public Object deserialize(InputStream inputStream) {
+      try (ObjectInputStream ois = new ObjectInputStream(inputStream)) {
+        return ois.readObject();
+      } catch (Exception ignored) {
+        return null;
+      }
+    }
   }
 }
